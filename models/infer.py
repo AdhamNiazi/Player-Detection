428dd3615cbab9d3df08a0505dd43f3cf5d45580
Adham Niazi <adham.k.niazi@gmail.com>
2024-05-20 18:21:29 -0700

    Initial File Structural Commit

---
+++ models/infer.py
@@ -0,0 +1,4 @@
+import cv2
+import torch
+import matplotlib.pyplot as plt
+from models.simple_object_detector import SimpleObjectDetector
---
+++ models/simple_object_detector.py
@@ -0,0 +1,8 @@
+import os
+import json
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import cv2
