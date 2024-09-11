88a6a1ed33ce378bcf24047e942c2b9758782188
AdhamNiazi <66962275+AdhamNiazi@users.noreply.github.com>
2024-09-11 16:15:55 -0700

    Delete models/train.py

--- models/train.py
+++
@@ -1,5 +0,0 @@
-import torch
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from models.simple_object_detector import PlayerDataset, SimpleObjectDetector
-import torch.nn.functional as F
